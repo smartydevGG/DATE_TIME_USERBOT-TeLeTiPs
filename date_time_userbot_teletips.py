e9ec2f1a63207f308ade566c4f432cea2355a06d
smartydevGG <110179855+smartydevGG@users.noreply.github.com>
2022-08-21 16:55:30 +0530

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -32,22 +32,10 @@ async def main_teletips():
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
                 Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
                 Date_teletips = TimeZone_teletips.strftime("%b %d") 
-                Image_teletips = Image.open("image.jpg")
-                Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
-                Image_text_teletips = f"{Time_teletips}"
-                Image_edit_teletips = ImageDraw.Draw(Image_teletips)
-                Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
-                Image_teletips.save("Image_final_teletips.jpg")
+                
                 await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}")
-                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
-                me = await Date_Time_Userbot_teletips.get_me()
-                photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
-                try:
-                    await Date_Time_Userbot_teletips.delete_profile_photos(photos[1].file_id)
-                except Exception:
-                    pass        
-                print("Profile Updated!")
-            await asyncio.sleep(60)     
+              
+            await asyncio.sleep(10)     
     except FloodWait as e:
         await asyncio.sleep(e.x)         
 
