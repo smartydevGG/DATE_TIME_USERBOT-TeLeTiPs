58317b6011b3e86cd8c0a4c1371d014a66dcafa6
smartydevGG <110179855+smartydevGG@users.noreply.github.com>
2022-08-21 11:44:06 +0530

    Update quotes_teletips.py

--- lists_teletips/quotes_teletips.py
+++ lists_teletips/quotes_teletips.py
@@ -4,5 +4,6 @@ quotes_teletips = [
 "എവിടെ ഒന്നും ഇല്ല mwonse ",
 "Online all day.. no sleep ",
 "La la la..."
-"Bio എഴുതാന്നൊന്നും എന്നിക് വയ്യ"
+"Bio എഴുതാന്നൊന്നും എന്നിക് വയ്യ",
+"A normal human being"
 ]
